0945fd2829edec912c7c8d845c6ad3e8a0f02b26
Lacey Stein <lacey.stein@stonybrook.edu>
2020-03-01 17:54:24 -0500

    Can select Fix overlapping precincts and a marker showing an overlapping precinct will pop up Cleaned up marker.js

--- src/marker.js
+++ src/marker.js
@@ -1,36 +1,35 @@
 /* Add a polygon to the map */
 /* TODO: Implement polygon drawing by using this code and the below code that gets X, Y coord
     of mouse click? */
-var p1 = new L.LatLng(34, -126),
-    p2 = new L.LatLng(40, -125),
-    p3 = new L.LatLng(36, -124),
-    polygonPoints = [p1, p2, p3],
-    polygon = new L.Polygon(polygonPoints);
+function addPolygon(){
+    var p1 = new L.LatLng(34, -126),
+        p2 = new L.LatLng(40, -125),
+        p3 = new L.LatLng(36, -124),
+        polygonPoints = [p1, p2, p3],
+        polygon = new L.Polygon(polygonPoints);
 
-map.addLayer(polygon);
+    map.addLayer(polygon);
+}
 
 function addMarker() {
-    map.on('click', onMapClick);
+    var markerLocation = new L.LatLng(39.657, -77.575),
+        marker = new L.Marker(markerLocation);
 
-    var popup = new L.Popup();
+    marker.bindTooltip("Overlapping precincts",
+        {
+            permanent: true,
+            direction: 'right'
+        });
+    map.addLayer(marker);
+}
 
-    /* On map click at a marker and prompt for label and display where you clicked */
+/* On map click displays string for where you clicked */
+function viewCursorLocation(){
     function onMapClick(e) {
         var latlngStr = '(' + e.latlng.lat.toFixed(3) + ', ' + e.latlng.lng.toFixed(3) + ')';
+        map.on('click', onMapClick);
 
-        var markerLocation = new L.LatLng(e.latlng.lat, e.latlng.lng),
-            marker = new L.Marker(markerLocation);
-
-        var label = prompt("Enter marker label:");
-
-        map.addLayer(marker);
-        marker.bindTooltip(label,
-            {
-                permanent: true,
-                direction: 'right'
-            });
-
-
+        var popup = new L.Popup();
         popup.setLatLng(e.latlng);
         popup.setContent("You clicked the map at " + latlngStr);
         map.openPopup(popup);
